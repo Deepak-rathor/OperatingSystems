889e44488ba48a3442198a5ef48ff160bdb4e5c8
Simon Kafader <simon.kafader@students.unibe.ch>
2017-02-23 18:56:40 +0100

    solved fibonacci task

--- Operating Systems/Series 0/fib.c
+++ Operating Systems/Series 0/fib.c
@@ -4,12 +4,21 @@
 #include <stdint.h>
 
 int fib(int a) {
-	/* TODO: implementation */
+	if(a==1){
+		return 1;
+	} else if(a==0){
+		return 0;
+	}else{
+		return fib(a-2)+fib(a-1);
+	}
 }
 
 int main(void)
 {
-	/* TODO: implementation */
+	int index;
+	printf("Please enter which fibonacci number you want:\n");
+	scanf("%d",&index);
+	printf("The %d. fibonacci number is %d",index,fib(index));
 
 	return 0;
 }
