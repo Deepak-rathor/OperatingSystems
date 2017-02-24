ef0b32de30882e7d1c5f7f77925a2512369a48a9
Simon Kafader <simon.kafader@students.unibe.ch>
2017-02-24 10:58:41 +0100

    solved task 5

--- Operating Systems/Series 0/cmake-build-debug/cmake_install.cmake
+++ Operating Systems/Series 0/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/Eigene Dokumente/C Projects/Operating Systems/Series 0
+# Install script for directory: C:/Users/smoen/Documents/Studium/Informatik/Betriebssysteme/OperatingSystems/Operating Systems/Series 0
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/Eigene Dokumente/C Projects/Operating Systems/Series 0/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/smoen/Documents/Studium/Informatik/Betriebssysteme/OperatingSystems/Operating Systems/Series 0/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
