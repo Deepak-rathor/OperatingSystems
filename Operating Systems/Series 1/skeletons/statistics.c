f6afd0438b98ca80c585ec24e69562f81b1b6d2a
Simon Kafader <simon.kafader@students.unibe.ch>
2017-03-09 15:37:10 +0100

    may have solved the task, requires testing in working enviroment

--- Operating Systems/Series 1/skeletons/statistics.c
+++ Operating Systems/Series 1/skeletons/statistics.c
@@ -12,16 +12,35 @@ int len;
 
 void *average(void *val) {
 	/* TODO */
+    avg=(val[0]+val[1]+val[2])/3;
 	return NULL;
 }
 
 void *minimum(void *val) {
 	/* TODO */
+    if(val[0]<val[1]){
+        min=val[0];
+    } else{
+        min = val[1];
+    }
+
+    if(val[2]<min){
+        min = val[2];
+    }
 	return NULL;
 }
 
 void *maximum(void *val) {
 	/* TODO */
+    if(val[0]>val[1]){
+        max=val[0];
+    }else {
+        max = val[1];
+    }
+
+    if(val[2]>max){
+        max=val[2];
+    }
 	return NULL;
 }
 
@@ -32,12 +51,20 @@ int main(int argc, char *argv[])
 	pthread_t thread1;
 	pthread_t thread2;
 
+	array = malloc(sizeof(int)*3);
+	printf("Please enter the three numbers to be used");
+	scanf("%d, %d, %d",array[0], array[1],array[2]);
+
 	if (argc < 3) {
 		printf("You have to supply at least 2 integers as arguments!\n");
 		return 0;
 	}
 
 	/* TODO */
+
+    HANDLE thread_0 = CreateThread(NULL,0,average(array),NULL,0,NULL);
+    HANDLE thread_1 = CreateThread(NULL,0,minimum(array),NULL,0,NULL);
+    HANDLE thread_2 = CreateThread(NULL,0,average(array),NULL,0,NULL);
 
 	printf("Average: %f\n" , avg);
 	printf("Maximum: %i\n" , max);
