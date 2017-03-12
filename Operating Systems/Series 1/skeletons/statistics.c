38f9501925e74b3898f3bb68b2bc6f51ea810198
Simon Kafader <simon.kafader@students.unibe.ch>
2017-03-13 00:04:17 +0100

    did some things that dont work but look better than before, core dumped error

--- Operating Systems/Series 1/skeletons/statistics.c
+++ Operating Systems/Series 1/skeletons/statistics.c
@@ -11,64 +11,84 @@ float avg;
 int len;
 
 void *average(void *val) {
-	/* TODO */
-    avg=(val[0]+val[1]+val[2])/3;
-	return NULL;
+    /* TODO */
+    int count;
+    int* numbers = (int*) val;
+    for(count=1;count<numbers[0];count++){
+        avg+=numbers[count];
+    }
+    avg/numbers[0];
+    return NULL;
 }
 
 void *minimum(void *val) {
-	/* TODO */
-    if(val[0]<val[1]){
-        min=val[0];
-    } else{
-        min = val[1];
+    /* TODO */
+
+    int count;
+    int* numbers = (int*) val;
+    min=numbers[1];
+
+    for(count=2;count<numbers[0];count++){
+        if(numbers[count]>min){
+            min=numbers[count];
+        }
     }
 
-    if(val[2]<min){
-        min = val[2];
-    }
-	return NULL;
+
+    return NULL;
 }
 
 void *maximum(void *val) {
-	/* TODO */
-    if(val[0]>val[1]){
-        max=val[0];
-    }else {
-        max = val[1];
-    }
+    /* TODO */
+    int count;
+    int* numbers = (int*) val;
+    max=numbers[1];
 
-    if(val[2]>max){
-        max=val[2];
+    for(count=2;count<numbers[0];count++){
+        if(numbers[count]>max){
+            max=numbers[count];
+        }
     }
-	return NULL;
+    return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-	int *array; // use this to store the command line parameters as integers
-	pthread_t thread0;
-	pthread_t thread1;
-	pthread_t thread2;
+    int *array; // use this to store the command line parameters as integers
+    pthread_t thread0;
+    pthread_t thread1;
+    pthread_t thread2;
 
-	array = malloc(sizeof(int)*3);
-	printf("Please enter the three numbers to be used");
-	scanf("%d, %d, %d",array[0], array[1],array[2]);
 
-	if (argc < 3) {
-		printf("You have to supply at least 2 integers as arguments!\n");
-		return 0;
-	}
 
-	/* TODO */
+    if (argc < 3) {
+        printf("You have to supply at least 2 integers as arguments!\n");
+        return 0;
+    }
 
-    HANDLE thread_0 = CreateThread(NULL,0,average(array),NULL,0,NULL);
-    HANDLE thread_1 = CreateThread(NULL,0,minimum(array),NULL,0,NULL);
-    HANDLE thread_2 = CreateThread(NULL,0,average(array),NULL,0,NULL);
+    array = malloc(sizeof(int)*argc); //too long
+    array[0]=argc-1;
+    int index;
+    for(index=1;index<argc-1;index++){
+        array[index]=atoi(argv[index]);
+    }
 
-	printf("Average: %f\n" , avg);
-	printf("Maximum: %i\n" , max);
-	printf("Minimum: %i\n" , min);
 
-	return 0;
+    /* TODO */
+
+
+    pthread_create(&thread0,NULL,average(array),(void*) array);
+    pthread_create(&thread1,NULL,minimum(array),(void*) array);
+    pthread_create(&thread2,NULL,maximum(array),(void*) array);
+
+    pthread_join(thread0,NULL);
+    pthread_join(thread1,NULL);
+    pthread_join(thread2,NULL);
+
+
+    printf("Average: %f\n" , avg);
+    printf("Maximum: %i\n" , max);
+    printf("Minimum: %i\n" , min);
+
+    return 0;
 }
