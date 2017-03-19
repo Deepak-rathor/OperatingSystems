608300df5f0d4eae4b81a22ee221590f6704ad9f
Simon Kafader <simon.kafader@students.unibe.ch>
2017-03-19 20:55:43 +0100

    Series 2 ready

---
+++ Operating Systems/Series 2/cmake-build-debug/CMakeFiles/Series_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Series_2.dir/main.c.obj"
+  "Series_2.pdb"
+  "Series_2.exe"
+  "Series_2.exe.manifest"
+  "libSeries_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Series_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
