3bbff76b8507ceb0a07ce1789e0320cfa193ed38
Simon Kafader <simon.kafader@students.unibe.ch>
2017-03-20 20:07:53 +0100

    solved task a and b

--- Operating Systems/Series 2/cmake-build-debug/CMakeFiles/Series_2.dir/DependInfo.cmake
+++ Operating Systems/Series 2/cmake-build-debug/CMakeFiles/Series_2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "E:/Eigene Dokumente/C Projects/Operating Systems/Series 2/main.c" "E:/Eigene Dokumente/C Projects/Operating Systems/Series 2/cmake-build-debug/CMakeFiles/Series_2.dir/main.c.obj"
+  "C:/Users/smoen/Documents/Studium/Informatik/Betriebssysteme/OperatingSystems/Operating Systems/Series 2/main.c" "C:/Users/smoen/Documents/Studium/Informatik/Betriebssysteme/OperatingSystems/Operating Systems/Series 2/cmake-build-debug/CMakeFiles/Series_2.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- Operating Systems/Series 2/cmake-build-debug/cmake_install.cmake
+++ Operating Systems/Series 2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/Eigene Dokumente/C Projects/Operating Systems/Series 2
+# Install script for directory: C:/Users/smoen/Documents/Studium/Informatik/Betriebssysteme/OperatingSystems/Operating Systems/Series 2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/Eigene Dokumente/C Projects/Operating Systems/Series 2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/smoen/Documents/Studium/Informatik/Betriebssysteme/OperatingSystems/Operating Systems/Series 2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
