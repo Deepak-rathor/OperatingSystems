26e43eed75b23393136d6cf218e4c4bd5941a6de
Simon Kafader <simon.kafader@students.unibe.ch>
2017-04-04 10:56:04 +0200

    introduced variable page_size for more general solution, wrote some comments for better understanding

--- Operating Systems/Series 3/main.c
+++ Operating Systems/Series 3/main.c
@@ -5,16 +5,33 @@
 #include <math.h>
 #include <stdint.h>
 
-/* TODO: Insert System Type here
- * System used: e.g. Linux MyDistro 32bit x86
+/*
+ * System used: Win10 64bit x86
+ *
+ * uint32_t (32 bit long integer) is chosen because of the rounding down
+ * property of the integer division and the size of 32 bit.
  */
 
-int calculate_page (uint32_t address) {
-    return (int) address /(pow(2,12));
+/*
+ * Added page_size as parameter for a cleaner, more general solution
+ *
+ * When you divide the address by the page_size and then round down
+ * you get the page.
+ * e.g. address 2, page_size 4 so it's on page 0,
+ * e.g. address 15, page_size 4 so it's on page 3
+ */
+int calculate_page (uint32_t address, int page_size) {
+    return (int) address /page_size;
 }
 
-int calculate_offset (uint32_t address , int page) {
-    return address - (page* (pow(2,12)));
+/*
+ * Added page_size as parameter for a cleaner, more general solution
+ *
+ * The offset is like the distance to the base address or the page.
+ * So we subtract the "address of the page" off the address to get it. 
+ */
+int calculate_offset (uint32_t address , int page, int page_size) {
+    return address - (page* page_size);
 }
 
 int main(int argc, char *argv[])
@@ -22,6 +39,7 @@ int main(int argc, char *argv[])
     long long_input;
     uint32_t address;
     int offset , page;
+    int page_size=pow(2,12); // more general solution, pow(2,12) = 4kB
 
     if (argc != 2) {
         printf("You must provide a parameter as input!\n");
@@ -37,10 +55,8 @@ int main(int argc, char *argv[])
         }
     }
 
-    /* TODO: calculate values and generate correct output */
-
-    page = calculate_page(address);
-    offset = calculate_offset(address,page);
+    page = calculate_page(address, page_size);
+    offset = calculate_offset(address,page,page_size);
 
     printf("The address %i contains: \n page number= %i \n offset = %i", address, page, offset);
 
