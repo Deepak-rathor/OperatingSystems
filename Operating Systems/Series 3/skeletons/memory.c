91533529ec6f61fa700855b69d3dc4141f0e1b61
Simon Kafader <simon.kafader@students.unibe.ch>
2017-04-04 10:59:55 +0200

    copied code from main.c into memory.c

--- Operating Systems/Series 3/skeletons/memory.c
+++ Operating Systems/Series 3/skeletons/memory.c
@@ -3,40 +3,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdint.h>
 
-/* TODO: Insert System Type here
- * System used: e.g. Linux MyDistro 32bit x86
+/*
+ * System used: Win10 64bit x64
+ *
+ * uint32_t (32 bit long integer) is chosen because of the rounding down
+ * property of the integer division and the size of 32 bit.
  */
 
-int calculate_page (/* TODO: Insert correct data type */ address) {
-	/* TODO: Implement */
+/*
+ * Added page_size as parameter for a cleaner, more general solution
+ *
+ * When you divide the address by the page_size and then round down
+ * you get the page.
+ * e.g. address 2, page_size 4 so it's on page 0,
+ * e.g. address 15, page_size 4 so it's on page 3
+ */
+int calculate_page (uint32_t address, int page_size) {
+    return (int) address /page_size;
 }
 
-int calculate_offset (/* TODO: Insert correct data type */ address , int page) {
-	/* TODO: Implement */
+/*
+ * Added page_size as parameter for a cleaner, more general solution
+ *
+ * The offset is like the distance to the base address or the page.
+ * So we subtract the "address of the page" off the address to get it.
+ */
+int calculate_offset (uint32_t address , int page, int page_size) {
+    return address - (page* page_size);
 }
 
 int main(int argc, char *argv[])
 {
-	long long_input;
-	/* TODO: Insert correct data type */ address;
-	int offset , page;
+    long long_input;
+    uint32_t address;
+    int offset , page;
+    int page_size=pow(2,12); // more general solution, pow(2,12) = 4kB
 
-	if (argc != 2) {
-		printf("You must provide a parameter as input!\n");
-		return -1;
-	} else {
-		long_input = atol(argv[1]);
-		long supremum = pow(2 , 8*sizeof(/* TODO: Insert correct data type */));
-		if (long_input < 0 || long_input >= supremum) {
-			printf("You must enter a valid 32bit address as parameter!\n");
-			return -1;
-		} else {
-			address = (/* TODO: Insert correct data type */) long_input;
-		}
-	}
+    if (argc != 2) {
+        printf("You must provide a parameter as input!\n");
+        return -1;
+    } else {
+        long_input = atol(argv[1]);
+        double supremum = pow(2 , 8*sizeof(uint32_t));
+        if (long_input < 0 || long_input >= supremum) {
+            printf("You must enter a valid 32bit address as parameter!\n");
+            return -1;
+        } else {
+            address = (uint32_t) long_input;
+        }
+    }
 
-	/* TODO: calculate values and generate correct output */
+    page = calculate_page(address, page_size);
+    offset = calculate_offset(address,page,page_size);
 
-	return 0;
+    printf("The address %i contains: \n page number= %i \n offset = %i", address, page, offset);
+
+    return 0;
 }
